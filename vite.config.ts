e5fee59d7ad4b8ac6d80da3c5414802b1b0bb5b1
Teetow <teetow@gmail.com>
2023-07-22 19:41:41 +0200

    fix: enable sharedbufferarray on gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
+import { execSync } from "child_process";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/master/",
-  plugins: [react(), crossOriginIsolation()],
+  plugins: [
+    react(),
+    crossOriginIsolation(),
+    {
+      writeBundle() {
+        execSync("cp ./coi-serviceworker.min.js ./dist");
+      },
+    },
+  ],
 });
