133c8548c2058b3f9216ba9241e6dab76f92b592
Teetow <teetow@gmail.com>
2023-07-27 13:26:16 +0200

    wip: more debugging

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,23 @@
+import { FFmpeg } from "@ffmpeg/ffmpeg";
+import { toBlobURL } from "@ffmpeg/util";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+
+const baseUrl = "https://unpkg.com/@ffmpeg/core@0.12.1/dist/umd";
+
+const testFfmpeg = async () => {
+  const ffmpeg = new FFmpeg();
+  console.log(ffmpeg);
+  await ffmpeg.load({
+    coreURL: await toBlobURL(`${baseUrl}/ffmpeg-core.js`, "text/javascript"),
+    wasmURL: await toBlobURL(`${baseUrl}/ffmpeg-core.wasm`, "application/wasm"),
+  });
+  console.log("loaded");
+  console.log(ffmpeg);
+};
+testFfmpeg();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
-import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
-import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
-import { execSync } from "child_process";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/master/",
-  plugins: [
-    react(),
-    crossOriginIsolation(),
-    {
-      writeBundle() {
-        execSync("cp ./coi-serviceworker.min.js ./dist");
-      },
-    },
-  ],
+  plugins: [react()],
   test: {
     environment: "jsdom",
     root: "src/",
