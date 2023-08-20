de922c843167602ee6d9c8a326e5d65bdcac7ea8
Teetow <teetow@gmail.com>
2023-08-21 01:35:22 +0200

    chore: target only the most [ADVANCED] browsers

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   // base: "/master/",
   plugins: [react()],
+  build: {
+    target: "esnext",
+  },
   optimizeDeps: {
     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
   },
