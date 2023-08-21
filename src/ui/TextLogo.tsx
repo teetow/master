ac8b6f4500141422e996abb52e4d9914e0376498
Teetow <teetow@gmail.com>
2023-08-21 02:00:07 +0200

    fix: remove bad logo, insert good logo

--- src/ui/TextLogo.tsx
+++ src/ui/TextLogo.tsx
@@ -1,12 +1,128 @@
-const logoText = String.raw`
- ___     ______  ______  ______
-/\  \   /\     \/\  \  \/\     \        __  ____ ____ ____ _____ ____ ____
-\ \  \  \ \  \  \ \  \  \ \  \  \      /  |/   //   // __//_  _// __//    |
- \ \  \___ \  \  \ \  \  \ \  \  \    /       // ' /__  /  / / / _/ /  ' /
-  \ \_____\ \_____\ \_____\ \____/   /__/ |__//_/_//___/  /_/ /___//__/|_|
-   \/_____/ /_____/ /_____/ /___/    \  \  \ \\ \ \\   \  \ \ \   \\  \\ \
-`;
+// const logoText = String.raw`
+//  ___     ______  ______  ______
+// /\  \   /\     \/\  \  \/\     \        __  ____ ____ ____ _____ ____ ____
+// \ \  \  \ \  \  \ \  \  \ \  \  \      /  |/   //   // __//_  _// __//    |
+//  \ \  \___ \  \  \ \  \  \ \  \  \    /       // ' /__  /  / / / _/ /  ' /
+//   \ \_____\ \_____\ \_____\ \____/   /__/ |__//_/_//___/  /_/ /___//__/|_|
+//    \/_____/ /_____/ /_____/ /___/    \  \  \ \\ \ \\   \  \ \ \   \\  \\ \
+// `;
+
+// export default function TextLogo() {
+//   return <>{logoText.replace(/ /g, String.fromCharCode(160))}</>;
+// }
 
 export default function TextLogo() {
-  return <>{logoText.replace(/ /g, String.fromCharCode(160))}</>;
+  return (
+    <>
+      <svg viewBox="11.564 8.345 596.924 82" width="596.924" height="82" fill="#ccc">
+        <path d="M19.563 8.345h8v2h-8v-2zM595.489 74.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM579.491 74.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM571.492 74.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM547.495 74.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM539.496 74.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM507.5 74.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM491.502 74.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM475.504 74.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM451.507 74.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM419.512 74.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM411.513 74.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM395.515 74.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM379.517 74.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM371.518 74.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM355.52 74.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM331.523 74.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM307.526 74.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM267.531 86.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4z" />
+        <path d="M259.532 88.345h8v2h-8v-2z" />
+        <path d="M251.533 88.345h8v2h-8v-2z" />
+        <path d="M243.534 88.345h8v2h-8v-2zM235.535 86.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM219.537 86.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4z" />
+        <path d="M211.538 88.345h8v2h-8v-2z" />
+        <path d="M203.539 88.345h8v2h-8v-2z" />
+        <path d="M195.54 88.345h8v2h-8v-2z" />
+        <path d="M187.541 88.345h8v2h-8v-2z" />
+        <path d="M179.542 88.345h8v2h-8v-2zM171.543 86.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM155.546 86.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4z" />
+        <path d="M147.547 88.345h8v2h-8v-2z" />
+        <path d="M139.548 88.345h8v2h-8v-2z" />
+        <path d="M131.549 88.345h8v2h-8v-2z" />
+        <path d="M123.55 88.345h8v2h-8v-2z" />
+        <path d="M115.551 88.345h8v2h-8v-2zM107.552 86.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM91.554 86.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4z" />
+        <path d="M83.555 88.345h8v2h-8v-2z" />
+        <path d="M75.556 88.345h8v2h-8v-2z" />
+        <path d="M67.557 88.345h8v2h-8v-2z" />
+        <path d="M59.558 88.345h8v2h-8v-2z" />
+        <path d="M51.559 88.345h8v2h-8v-2zM43.56 86.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4z" />
+        <path d="M35.561 74.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM598.489 58.345h2v16h-2v-16zM587.49 72.345h8v2h-8v-2zM582.491 58.345h2v16h-2v-16zM571.492 70.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4z" />
+        <path d="M563.493 72.345h8v2h-8v-2z" />
+        <path d="M555.494 72.345h8v2h-8v-2zM547.495 70.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM539.496 70.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4z" />
+        <path d="M531.497 72.345h8v2h-8v-2z" />
+        <path d="M523.498 72.345h8v2h-8v-2z" />
+        <path d="M515.499 72.345h8v2h-8v-2zM507.5 70.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM491.502 70.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4z" />
+        <path d="M483.503 72.345h8v2h-8v-2zM475.504 70.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM451.507 70.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4z" />
+        <path d="M443.508 72.345h8v2h-8v-2z" />
+        <path d="M435.51 72.345h8v2h-8v-2z" />
+        <path d="M427.511 72.345h8v2h-8v-2zM419.512 70.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM411.513 70.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4z" />
+        <path d="M403.514 72.345h8v2h-8v-2zM395.515 70.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4z" />
+        <path d="M387.516 72.345h8v2h-8v-2zM379.517 70.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM371.518 70.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4z" />
+        <path d="M363.519 72.345h8v2h-8v-2z" />
+        <path d="M355.52 72.345h8v2h-8v-2zM350.521 58.345h2v16h-2v-16zM331.523 70.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4z" />
+        <path d="M323.524 72.345h8v2h-8v-2z" />
+        <path d="M315.525 72.345h8v2h-8v-2zM307.526 70.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM275.53 70.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4z" />
+        <path d="M267.531 72.345h8v2h-8v-2z" />
+        <path d="M259.532 72.345h8v2h-8v-2z" />
+        <path d="M251.533 72.345h8v2h-8v-2z" />
+        <path d="M243.534 72.345h8v2h-8v-2z" />
+        <path d="M235.535 58.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM219.537 58.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM211.538 72.345h8v2h-8v-2z" />
+        <path d="M203.539 72.345h8v2h-8v-2z" />
+        <path d="M195.54 72.345h8v2h-8v-2z" />
+        <path d="M187.541 72.345h8v2h-8v-2z" />
+        <path d="M179.542 72.345h8v2h-8v-2z" />
+        <path d="M171.543 58.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM155.546 58.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM147.547 72.345h8v2h-8v-2z" />
+        <path d="M139.548 72.345h8v2h-8v-2z" />
+        <path d="M131.549 72.345h8v2h-8v-2z" />
+        <path d="M123.55 72.345h8v2h-8v-2z" />
+        <path d="M115.551 72.345h8v2h-8v-2z" />
+        <path d="M107.552 58.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM91.554 58.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM83.555 72.345h8v2h-8v-2z" />
+        <path d="M75.556 72.345h8v2h-8v-2z" />
+        <path d="M67.557 72.345h8v2h-8v-2z" />
+        <path d="M59.558 72.345h8v2h-8v-2z" />
+        <path d="M51.559 72.345h8v2h-8v-2z" />
+        <path d="M43.56 58.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM27.562 58.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM595.489 54.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM581.491 46.345h1v-4h2v4h-1v2h-2v-2zM555.494 54.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM539.496 54.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4z" />
+        <path d="M531.497 56.345h8v2h-8v-2zM515.499 54.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM499.501 54.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM483.503 54.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM459.506 54.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM435.51 56.345h8v2h-8v-2z" />
+        <path d="M427.511 56.345h8v2h-8v-2zM419.512 54.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM405.514 46.345h1v-4h2v4h-1v2h-2v-2zM387.516 54.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM379.517 54.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM315.525 54.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM275.53 42.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM251.533 42.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM227.536 42.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM211.538 42.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM187.541 42.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM163.544 42.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM147.547 42.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM123.55 42.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM99.553 42.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM83.555 56.345h8v2h-8v-2z" />
+        <path d="M75.556 56.345h8v2h-8v-2z" />
+        <path d="M67.557 56.345h8v2h-8v-2z" />
+        <path d="M59.558 42.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM35.561 42.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM19.563 42.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM606.488 26.345h2v16h-2v-16zM563.493 38.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM555.494 38.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4z" />
+        <path d="M547.495 40.345h8v2h-8v-2z" />
+        <path d="M539.496 40.345h8v2h-8v-2zM523.498 38.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM515.499 38.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4z" />
+        <path d="M507.5 40.345h8v2h-8v-2zM483.503 40.345h8v2h-8v-2zM475.504 38.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM467.505 38.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4z" />
+        <path d="M459.506 40.345h8v2h-8v-2z" />
+        <path d="M451.507 40.345h8v2h-8v-2zM435.509 38.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM427.51 38.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM395.515 38.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM387.516 38.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM355.52 38.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM350.521 26.345h2v16h-2v-16zM323.524 38.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM267.531 26.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM243.534 26.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM219.537 26.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM203.539 26.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM179.542 26.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM155.545 26.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM139.547 26.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM115.551 26.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM91.554 26.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM75.556 26.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM51.559 26.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM27.562 26.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM11.564 26.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM595.489 24.345h8v2h-8v-2z" />
+        <path d="M587.49 24.345h8v2h-8v-2z" />
+        <path d="M579.491 24.345h8v2h-8v-2z" />
+        <path d="M571.492 24.345h8v2h-8v-2zM555.494 24.345h8v2h-8v-2z" />
+        <path d="M547.495 24.345h8v2h-8v-2z" />
+        <path d="M539.496 24.345h8v2h-8v-2z" />
+        <path d="M531.497 24.345h8v2h-8v-2zM515.499 24.345h8v2h-8v-2z" />
+        <path d="M507.5 24.345h8v2h-8v-2z" />
+        <path d="M499.501 24.345h8v2h-8v-2z" />
+        <path d="M491.502 24.345h8v2h-8v-2z" />
+        <path d="M483.503 24.345h8v2h-8v-2zM467.505 24.345h8v2h-8v-2z" />
+        <path d="M459.506 24.345h8v2h-8v-2z" />
+        <path d="M451.507 24.345h8v2h-8v-2z" />
+        <path d="M443.508 24.345h8v2h-8v-2zM427.51 24.345h8v2h-8v-2z" />
+        <path d="M419.511 24.345h8v2h-8v-2z" />
+        <path d="M411.512 24.345h8v2h-8v-2z" />
+        <path d="M403.514 24.345h8v2h-8v-2zM387.516 24.345h8v2h-8v-2z" />
+        <path d="M379.517 24.345h8v2h-8v-2z" />
+        <path d="M371.518 24.345h8v2h-8v-2z" />
+        <path d="M363.519 24.345h8v2h-8v-2zM339.522 24.345h8v2h-8v-2z" />
+        <path d="M331.523 24.345h8v2h-8v-2zM259.532 10.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM211.538 10.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2z" />
+        <path d="M203.539 22.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4z" />
+        <path d="M195.54 10.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM171.543 10.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM147.546 10.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2z" />
+        <path d="M139.547 22.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4z" />
+        <path d="M131.549 10.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM83.555 10.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2z" />
+        <path d="M75.556 22.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM43.56 10.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2zM19.563 10.345h2v2h1v2h1v2h1v2h1v2h1v2h1v4h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2h-1v-2z" />
+        <path d="M11.564 22.345h1v-2h1v-2h1v-2h1v-2h1v-2h1v-2h2v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v2h-1v-4zM251.533 8.345h8v2h-8v-2z" />
+        <path d="M243.534 8.345h8v2h-8v-2z" />
+        <path d="M235.535 8.345h8v2h-8v-2z" />
+        <path d="M227.536 8.345h8v2h-8v-2z" />
+        <path d="M219.537 8.345h8v2h-8v-2z" />
+        <path d="M211.538 8.345h8v2h-8v-2zM187.541 8.345h8v2h-8v-2z" />
+        <path d="M179.542 8.345h8v2h-8v-2z" />
+        <path d="M171.543 8.345h8v2h-8v-2z" />
+        <path d="M163.544 8.345h8v2h-8v-2z" />
+        <path d="M155.545 8.345h8v2h-8v-2z" />
+        <path d="M147.546 8.345h8v2h-8v-2zM123.549 8.345h8v2h-8v-2z" />
+        <path d="M115.551 8.345h8v2h-8v-2z" />
+        <path d="M107.552 8.345h8v2h-8v-2z" />
+        <path d="M99.553 8.345h8v2h-8v-2z" />
+        <path d="M91.554 8.345h8v2h-8v-2z" />
+        <path d="M83.555 8.345h8v2h-8v-2zM35.561 8.345h8v2h-8v-2z" />
+        <path d="M27.562 8.345h8v2h-8v-2z" />
+      </svg>
+    </>
+  );
 }
